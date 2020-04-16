e6686453b7a97cc7a67fd1c948a2e74a1b5a6e94
Heshanibandaranayake <heshanibandaranayake@gmail.com>
2020-04-16 10:37:33 +0530

    few changes

--- UI/script.js
+++ UI/script.js
@@ -1,4 +1,8 @@
-// Material Select Initialization
-$(document).ready(function() {
-$('.mdb-select').materialSelect();
-});
+// Material Select Initializatio
+document.multiselect('#testSelect1')
+  .setCheckBoxClick("checkboxAll", function(target, args) {
+    console.log("Checkbox 'Select All' was clicked and got value ", args.checked);
+  })
+  .setCheckBoxClick("1", function(target, args) {
+    console.log("Checkbox for item with value '1' was clicked and got value ", args.checked);
+  });
